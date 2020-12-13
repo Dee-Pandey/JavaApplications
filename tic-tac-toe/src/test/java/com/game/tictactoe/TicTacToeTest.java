f85dc14096b4b7caba8a48750b171387d295cd37
Deepak Pandey <Deepak Pandey@Deepak>
2020-12-13 20:41:13 +0530

    Introduced a new test case to test that Player symbols X and O are present only at even indices of the 2D board array i.e board[even][even] always contains either 'X' or 'O' or ' '. This pattern constructs and maintains the GameBoard's structure.

--- tic-tac-toe/src/test/java/com/game/tictactoe/TicTacToeTest.java
+++ tic-tac-toe/src/test/java/com/game/tictactoe/TicTacToeTest.java
@@ -49,10 +49,27 @@ public class TicTacToeTest
 		Game game = new Game();
 		game.createGameBoard(3);
 		char [][] board = game.getGameBoard().getGameGrid();
-		   System.out.println(board[0][1]);
-		   assertEquals(board[0][1], '|');
-		   assertEquals(board[1][0], '-');
-		   assertEquals(board[1][1], '+');
+		   assertEquals('|',board[0][1]);
+		   assertEquals('-',board[1][0]);
+		   assertEquals('+',board[1][1]);
+	}
+	@Test
+	public void testPlayerPlacementsInBoard()
+	{
+		Game game = new Game();
+		game.createGameBoard(2);
+		char [][] board = game.getGameBoard().getGameGrid();
+		game.getGameBoard().fillPlayerSymbolAtPosition(1, 1, PlayerSymbol.X);
+		game.getGameBoard().fillPlayerSymbolAtPosition(1, 2, PlayerSymbol.O);
+		game.getGameBoard().fillPlayerSymbolAtPosition(2, 1, PlayerSymbol.X);
+		assertEquals(PlayerSymbol.X.toString(),Character.toString(board[0][0]));
+		assertEquals(PlayerSymbol.O.toString(),Character.toString(board[0][2]));
+		assertEquals(PlayerSymbol.X.toString(),Character.toString(board[2][0]));
+		assertEquals(false, (board[1][0]=='X')||board[1][0]=='O'||board[1][0]==' ');
+		assertEquals(false, (board[1][1]=='X')||board[1][1]=='O'||board[1][1]==' ');
+		assertEquals(false, (board[1][2]=='X')||board[1][2]=='O'||board[1][2]==' ');
+		assertEquals(false, (board[2][1]=='X')||board[2][1]=='O'||board[2][1]==' ');
+		
 	}
 	
 	@Test
